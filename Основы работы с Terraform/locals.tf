beb49e264865272de84fb3390d6da4ae76430fef
Dimarkle <118626944+Dimarkle@users.noreply.github.com>
2023-09-24 15:13:26 +0300

    Delete Основы работы с Terraform directory

--- Основы работы с Terraform/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
- web_project = "${var.vm_name_instance.project}-${var.vm_name_instance.dev}-${var.vm_name_instance.platf}-web"
- db_project = "${var.vm_name_instance.project}-${var.vm_name_instance.dev}-${var.vm_name_instance.platf}-db"
-}
